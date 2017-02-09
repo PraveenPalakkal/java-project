18336bc8f073044d8f2f35a0df8d522342c6516b
Praveen Palakkal <praveen.pp79@gmail.com>
2017-02-09 12:21:57 +0530

    my first java programs

---
+++ Hellosample.java
@@ -0,0 +1,9 @@
+public class Hellosample
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println();
+		System.out.println("Hello and welcome");
+	}
+}
---
+++ sample.java
@@ -0,0 +1,8 @@
+public class Hellosample
+{
+	public static void main(String args[])
+	{
+		System.out.println();
+		System.out.println("Hello and welcome");
+	}
+}
